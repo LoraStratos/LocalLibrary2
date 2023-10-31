f353e2a1be9c9d0cff2dab9f7529f363dbf9db96
LoraStratos <LoraStratosSi@mail.ru>
2023-10-31 22:58:57 +0700

    незначительные изменения

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -9,6 +9,8 @@ import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required, permission_required
+from django.forms import ModelForm
+from .models import BookInstance
 
 
 class MyView(LoginRequiredMixin):
@@ -126,3 +128,16 @@ def renew_book_librarian(request, pk):
 
     return render(request, 'catalog/book_renew_librarian.html', context)
 
+class RenewBookModelForm(ModelForm):
+    def clean_due_back(self):
+        data = self.cleaned_data['due_back']
+        if data < datetime.date.today():
+            raise ValidationError(_('Invalid date - renewal in past'))
+        if data > datetime.date.today() + datetime.timedelta(weeks=4):
+            raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
+        return data
+    class Meta:
+        model = BookInstance
+        fields = ['due_back',]
+        labels = {'due_back': _('Renewal date'), }
+        help_texts = {'due_back': _('Enter a date between now and 4 weeks (default 3).'), }
