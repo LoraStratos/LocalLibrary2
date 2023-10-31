815d26f4345ccb6f7b510ecfd8746b3422f506e1
LoraStratos <LoraStratosSi@mail.ru>
2023-10-31 21:09:11 +0700

    незначительные изменения

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -9,9 +9,6 @@ class MyView(LoginRequiredMixin):
     redirect_field_name = 'redirect_to'
 
 def index(request):
-    """
-    Функция отображения для домашней страницы сайта.
-    """
     num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
@@ -55,3 +52,15 @@ class AuthorDetailView(generic.DetailView):
 def author_detail_view(request, primary_key):
     author = get_object_or_404(Author, pk=primary_key)
     return render(request, 'catalog/author_detail.html', context={'author': author})
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact='o')
+            .order_by('due_back')
+        )
