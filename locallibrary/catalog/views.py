49c6268ba8aa84208a453dc65912c8da1266835e
LoraStratos <LoraStratosSi@mail.ru>
2023-10-31 10:49:15 +0700

    file1

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -11,17 +11,8 @@ def index(request):
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()
     num_genres=Genre.objects.count()
-    num_search=SearchResultsView()
     return render(
         request,
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genres':num_genres},
     )
-
-class SearchResultsView(ListView):
-    model = Book
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        object_list = Book.objects.filter(
-            (title__icontains=query)
-        )
