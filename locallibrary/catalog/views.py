e6e963870cb7895738e961b0c09f692bcc861139
LoraStratos <LoraStratosSi@mail.ru>
2023-10-31 10:32:12 +0700

    file1

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -9,8 +9,9 @@ def index(request):
     num_instances=BookInstance.objects.all().count()
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()
+    num_genres=Genre.objects.count()
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genres':num_genres},
     )
